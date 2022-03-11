fd5fb64745d26f85c9b15c11e3fbc68e468d142f
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-03-11 17:02:48 +0900

    edit profile

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -10,6 +10,8 @@ import { LoginInput } from './dtos/login.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from './../auth/auth.guard';
 import { AuthUser } from 'src/auth/auth-user.decorator';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 
 @Resolver((of) => User)
 export class UsersResolver {
@@ -45,7 +47,40 @@ export class UsersResolver {
   }
 
   @Query((returns) => User)
+  @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User): User {
     return authUser;
+  }
+
+  @Query((returns) => UserProfileOutput)
+  @UseGuards(AuthGuard)
+  async user(@Args() { userId }: UserProfileInput): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userId);
+      if (!user) {
+        throw Error();
+      }
+      return { ok: true, user };
+    } catch (e) {
+      console.error(e);
+      return {
+        error: 'user not found',
+        ok: false,
+      };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation((returns) => EditProfileOutput)
+  async editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    try {
+      await this.usersService.editProfile(authUser.id, editProfileInput);
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error };
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,7 @@ import { compare } from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from './../jwt/jwt.service';
+import { EditProfileInput } from './dtos/edit-profile.dto';
 
 @Injectable()
 export class UsersService {
@@ -62,5 +63,19 @@ export class UsersService {
 
   async findById(id: number): Promise<User> {
     return this.users.findOne({ id });
+  }
+
+  async editProfile(
+    userId: number,
+    { email, password }: EditProfileInput,
+  ): Promise<User> {
+    const user = await this.users.findOne({ id: userId });
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    return this.users.save(user);
   }
 }
